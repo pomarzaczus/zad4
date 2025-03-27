7cd84595d9b914a64b55e0b6ca83064fbc6c055f
pomaraczus <buhahahahah052gmail.com>
2025-03-27 10:54:01 +0100

    jezu

--- zad4/Form1.Designer.cs
+++ zad4/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             button1 = new Button();
             pictureBox1 = new PictureBox();
+            button2 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -48,26 +53,94 @@
             pictureBox1.Location = new Point(524, 128);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(100, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(80, 349);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(80, 57);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(37, 34);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "90\r\n\r\n";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(80, 82);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(43, 19);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "180";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(80, 107);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(43, 19);
+            radioButton3.TabIndex = 5;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "270";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(416, 323);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 6;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(button2);
             Controls.Add(pictureBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private PictureBox pictureBox1;
+        private Button button2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Label label1;
     }
 }
--- zad4/Form1.cs
+++ zad4/Form1.cs
@@ -1,5 +1,6 @@
 using System.Data;
 using System;
+using System.Linq.Expressions;
 
 namespace zad4
 {
@@ -9,6 +10,7 @@ namespace zad4
         {
             InitializeComponent();
         }
+        int rorataeval = 0;
         public void loadImage(string filePath)
         {
             // SprawdŸ, czy plik istnieje
@@ -20,27 +22,27 @@ namespace zad4
             // Odczytaj zawartoœæ pliku CSV
 
             pictureBox1.Image = Image.FromFile(filePath);
-
+           
 
         }
         private void wczytaj_Click(object sender, EventArgs e)
         {
-            
 
-            
+
+
         }
 
         public void rotateImage()
         {
-
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
         }
         public void rotateImage1()
         {
-
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
         }
         public void rotateImage2()
         {
-
+            pictureBox1.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
         }
         public void invert()
         {
@@ -69,6 +71,49 @@ namespace zad4
                 // Wywo³anie funkcji wczytuj¹cej dane z pliku CSV
                 loadImage(openFileDialog1.FileName);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch (rorataeval)
+            {
+                case 90:
+                    label1.Text = rorataeval.ToString();
+                    rotateImage();
+                    break;
+                case 180:
+                    label1.Text = rorataeval.ToString();
+                    rotateImage1();
+                    break;
+                case 270:
+                    label1.Text = rorataeval.ToString();
+                    rotateImage2();
+                    break;
+                default:
+                    // code block
+                    break;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            rorataeval = 90;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            rorataeval = 180;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            rorataeval = 270;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = rorataeval.ToString();
         }
     }
 
