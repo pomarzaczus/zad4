662294efbe3872edbb84c0b6e38452fdb25da8ff
jagodazorza <kotkotowski080@gmail.com>
2025-03-27 10:24:36 +0100

    :(

--- zad4/Form1.Designer.cs
+++ zad4/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(80, 282);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(524, 128);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
     }
 }
--- zad4/Form1.cs
+++ zad4/Form1.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System;
+
 namespace zad4
 {
     public partial class Form1 : Form
@@ -6,5 +9,69 @@ namespace zad4
         {
             InitializeComponent();
         }
+        public void loadImage(string filePath)
+        {
+            // SprawdŸ, czy plik istnieje
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Plik xd nie istnieje.", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // Odczytaj zawartoœæ pliku CSV
+
+            pictureBox1.Image = Image.FromFile(filePath);
+
+
+        }
+        private void wczytaj_Click(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+
+        public void rotateImage()
+        {
+
+        }
+        public void rotateImage1()
+        {
+
+        }
+        public void rotateImage2()
+        {
+
+        }
+        public void invert()
+        {
+
+        }
+        public void greeeen()
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Wyœwietlenie okna dialogowego wyboru pliku CSV
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Pliki CSV (*.csv)|*.csv|Wszystkie pliki (*.*)|*.*";
+            openFileDialog1.Title = "Wybierz plik CSV do wczytania";
+            openFileDialog1.ShowDialog();
+            // Jeœli u¿ytkownik wybierze plik i zatwierdzi, wczytaj dane z pliku CSV
+            if (openFileDialog1.FileName != "")
+            {
+                // Wywo³anie funkcji wczytuj¹cej dane z pliku CSV
+                loadImage(openFileDialog1.FileName);
+            }
+        }
     }
+
+
+
 }
